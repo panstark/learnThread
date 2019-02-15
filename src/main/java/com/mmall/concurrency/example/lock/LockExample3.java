7fdd649fc1b4dbed575a7e37dace19f79eeb6137
starkpan <starkpan@163.com>
2019-02-15 10:57:15 +0800

    init

---
+++ src/main/java/com/mmall/concurrency/example/lock/LockExample3.java
@@ -0,0 +1,56 @@
+package com.mmall.concurrency.example.lock;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 写锁在写之前，不能有任何读锁，否则就等待。
+ * 如果有，则等待
+ */
+@Slf4j
+public class LockExample3 {
+
+    private final Map<String, Data> map = new TreeMap<>();
+
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private final Lock readLock = lock.readLock();
+
+    private final Lock writeLock = lock.writeLock();
+
+    public Data get(String key) {
+        readLock.lock();
+        try {
+            return map.get(key);
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public Set<String> getAllKeys() {
+        readLock.lock();
+        try {
+            return map.keySet();
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public Data put(String key, Data value) {
+        writeLock.lock();
+        try {
+            return map.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
+    }
+
+    class Data {
+
+    }
+}
